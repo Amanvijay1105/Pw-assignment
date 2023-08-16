f68ed35e212fdf2427c9104d599959061ac1e05c
Amanvijay1105 <120350316+Amanvijay1105@users.noreply.github.com>
2023-08-16 15:10:38 +0530

    Add files via upload

---
+++ basics/circum.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int radius = 10;
+    float circum = 2*3.1415*radius;
+    cout << "Circumference is :" << circum << endl;
+    return 0;
+}
